f3f937b756efb1b1b5f966a8182d3e83b7356157
Pedro Wanderley <peu-zico@hotmail.com>
2020-07-27 12:21:08 -0300

    refactory

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,11 +1,11 @@
-import { compare } from 'bcryptjs';
-import { sign } from 'jsonwebtoken';
-import { injectable, inject } from 'tsyringe';
+import { sign } from "jsonwebtoken";
+import { injectable, inject } from "tsyringe";
 
-import IUsersRepository from '../repositories/IUsersRepository';
-import AppError from '@shared/errors/AppError';
+import AppError from "@shared/errors/AppError";
+import IUsersRepository from "../repositories/IUsersRepository";
+import IHashProvider from "../providers/HashProvider/models/IHashProvider";
 
-import authConfig from '@config/auth';
+import authConfig from "@config/auth";
 import User from "../infra/typeorm/entities/User";
 
 interface IRequest {
@@ -21,26 +21,32 @@ interface IResponse {
 @injectable()
 class AuthenticateUserService {
   constructor(
-    @inject('UsersRepository')
+    @inject("UsersRepository")
     private usersRepository: IUsersRepository,
+
+    @inject("HashProvider")
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-     throw new AppError('Incorrect email/password combination.', 401);
+      throw new AppError("Incorrect email/password combination.", 401);
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
-     throw new AppError('Incorrect email/password combination.', 401);
+      throw new AppError("Incorrect email/password combination.", 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
 
-    const token = sign({ }, secret, {
+    const token = sign({}, secret, {
       subject: user.id,
       expiresIn,
     });
